506abbfa9ef840066717b1e63d4d47448037cd11
ValaniAnjali <167547521+ValaniAnjali@users.noreply.github.com>
2024-06-24 01:11:38 -0700

    Add files via upload

---
+++ randomemojigenerator/script.js
@@ -0,0 +1,29 @@
+const emoji = document.querySelector('#emoji');
+const emojis=[
+    '😊',
+    '😍',
+    '🤣',
+    '😁',
+    '😜',
+    '😃',
+    '😎',
+    '😉',
+    '🤔',
+    '🤩',
+    '😝',
+    '🤪',
+    '😅',
+    '🙂',
+    '🤗',
+    '😺',
+    '😋',
+    '🤑',
+    '😇',
+    '🥳',
+    '🤠',
+    '🤭',
+
+];
+emoji.addEventListener('mouseover',()=>{
+    emoji.innerHTML=emojis[Math.floor(Math.random()*emojis.length)]
+})
